089b81e997b140083dd75242f722f481f032d658
minercool <zghidi@gmail.com>
2022-05-13 21:32:56 +0100

    updates

--- backend/adminController/grantUser.php
+++ backend/adminController/grantUser.php
@@ -4,12 +4,19 @@ if(!(isset($_SESSION['logged'])) || ($_SESSION['logged'] != true) || !(isset($_S
     session_destroy();
     header('Location: ../../frontend/login.php');
 }
+$x = 0;
 $id = $_GET['id'];
 $query = $conn->prepare("UPDATE users SET role = 'seller' WHERE id = '$id'");
 if($query->execute()){
-header('Location: ../../frontend/admin/sellers.php');
+    $query = $conn->prepare("INSERT INTO balance VALUES('','$id','$x')");
+    if($query->execute()){
+        header('Location: ../../frontend/admin/sellers.php');
+    }else{
+        echo '<h1>Internal error</h1>';
+    }
+    
 }else{
-    echo 'internal error';
+    echo '<h1>Internal error</h1>'; 
 }
 
 ?>
--- backend/adminController/revokeSeller.php
+++ backend/adminController/revokeSeller.php
@@ -7,7 +7,12 @@ if(!(isset($_SESSION['logged'])) || ($_SESSION['logged'] != true) || !(isset($_S
 $id = $_GET['id'];
 $query = $conn->prepare("UPDATE users SET role = 'user' WHERE id = '$id'");
 if($query->execute()){
-header('Location: ../../frontend/admin/sellers.php');
+    $query = $conn->prepare("DELETE FROM balance WHERE seller_id = '$id'");
+    if($query->execute()){
+        header('Location: ../../frontend/admin/sellers.php');
+    }else{
+        echo '<h1>Internal error</h1>';
+    }
 }else{
     echo 'internal error';
 }
